5650d04ff081f4d3f4b5f6ee06e0c8f5058d7edf
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-11 14:33:31 +0200

    added default code

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,19 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return (
+    <Map center={position} zoom={this.state.zoom}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
+  )
 }
 
 export default App
