e13ec98fb6b9f470e39c8032000f9c2eb0d539e2
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-11 14:40:59 +0200

    added height to the map

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     const position = [this.state.lat, this.state.lng]
 
     return (
-      <Map center={position} zoom={this.state.zoom}>
+      <Map className='map' center={position} zoom={this.state.zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
