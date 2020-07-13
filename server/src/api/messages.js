419cbf3e459e8d7d6df16f140122e838506ffa6c
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:46:19 +0200

    added message to the schema

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -3,6 +3,7 @@ const Joi = require('joi')
 
 const schema = Joi.object().keys({
   name: Joi.string().alphanum().min(1).max(100).required(),
+  message: Joi.string().alphanum().min(1).max(500).required(),
 })
 
 const router = express.Router()
