cfe333fffe0f69e3835b8bdf6b26503bdba5f084
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:45:32 +0200

    added name

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Joi = require('joi')
 
 const schema = Joi.object().keys({
-  name: Joi.string().alphanum().min(1).max(100).required()
+  name: Joi.string().alphanum().min(1).max(100).required(),
 })
 
 const router = express.Router()
