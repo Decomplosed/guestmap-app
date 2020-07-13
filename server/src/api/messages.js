59ceee65cbe002dad7f65f441ca19b2fd8007029
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:47:25 +0200

    added date to the schema

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -5,7 +5,8 @@ const schema = Joi.object().keys({
   name: Joi.string().alphanum().min(1).max(100).required(),
   message: Joi.string().alphanum().min(1).max(500).required(),
   latitude: Joi.number(),
-  longitude: Joi.number()
+  longitude: Joi.number(),
+  date: Joi.date(),
 })
 
 const router = express.Router()
