13c5f8b9e860e0bcbc4a9411cb240e502bb7d085
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:45:19 +0200

    started creating schema with Joi

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const Joi = require('joi')
 
+const schema = Joi.object().keys({
+  name: Joi.string().alphanum().min(1).max(100).required()
+})
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
