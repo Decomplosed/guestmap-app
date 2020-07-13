8fa4c0e4ec2e5228f29e52ceabf57e91e80808d7
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:51:04 +0200

    valudated lllatitude

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -4,7 +4,7 @@ const Joi = require('joi')
 const schema = Joi.object().keys({
   name: Joi.string().alphanum().min(1).max(100).required(),
   message: Joi.string().alphanum().min(1).max(500).required(),
-  latitude: Joi.number().required(),
+  latitude: Joi.number().required().min(-90).max(90),
   longitude: Joi.number().required(),
   date: Joi.date(),
 })
