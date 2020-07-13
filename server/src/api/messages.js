ec35eb2e009e58c24c3fa80590ea17d69b42c972
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:50:24 +0200

    turned latitude and longitude to required

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -4,8 +4,8 @@ const Joi = require('joi')
 const schema = Joi.object().keys({
   name: Joi.string().alphanum().min(1).max(100).required(),
   message: Joi.string().alphanum().min(1).max(500).required(),
-  latitude: Joi.number(),
-  longitude: Joi.number(),
+  latitude: Joi.number().required(),
+  longitude: Joi.number().required(),
   date: Joi.date(),
 })
 
