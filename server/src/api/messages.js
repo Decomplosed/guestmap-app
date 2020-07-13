ad1dcc9264e47e1f1dd28fc7ec69b9942411f33f
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:51:43 +0200

    validated max and min for longitude

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -4,8 +4,8 @@ const Joi = require('joi')
 const schema = Joi.object().keys({
   name: Joi.string().alphanum().min(1).max(100).required(),
   message: Joi.string().alphanum().min(1).max(500).required(),
-  latitude: Joi.number().required().min(-90).max(90),
-  longitude: Joi.number().required(),
+  latitude: Joi.number().min(-90).max(90).required(),
+  longitude: Joi.number().min(-180).max(180).required(),
   date: Joi.date(),
 })
 
