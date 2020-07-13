673c76658941efb371fd330b7d104b5cdb9dbb44
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:52:47 +0200

    removed date from schema

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -6,7 +6,6 @@ const schema = Joi.object().keys({
   message: Joi.string().alphanum().min(1).max(500).required(),
   latitude: Joi.number().min(-90).max(90).required(),
   longitude: Joi.number().min(-180).max(180).required(),
-  date: Joi.date(),
 })
 
 const router = express.Router()
