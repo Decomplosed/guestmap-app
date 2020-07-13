cadc48b36938ca7542d665a202a8919f93b1ca9c
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-13 11:46:38 +0200

    added latitude to the schema

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -4,6 +4,7 @@ const Joi = require('joi')
 const schema = Joi.object().keys({
   name: Joi.string().alphanum().min(1).max(100).required(),
   message: Joi.string().alphanum().min(1).max(500).required(),
+  latitude: Joi.number()
 })
 
 const router = express.Router()
