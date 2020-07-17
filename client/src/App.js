09a56efc6d482446912327aeadf247dbd65817c4
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-17 21:04:37 +0200

    declared messagesByLocation object

--- client/src/App.js
+++ client/src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
     fetch(API_URL)
       .then((res) => res.json())
       .then((messages) => {
+        const messagesByLocation = {}
         const messages = messages.reduce((all, message) => {}, [])
 
         this.setState({ messages })
