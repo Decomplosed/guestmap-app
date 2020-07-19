325b27919e91403dfa18627bcccb29c7ecc91bdb
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-19 09:27:32 +0200

    moved fetch to the separated function

---
+++ client/src/API.js
@@ -0,0 +1,19 @@
+function getMessages() {
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then((messages) => {
+      const haveSeenLocation = {}
+      return messages.reduce((all, message) => {
+        const key = `${message.latitude}${messages.longitude}`
+        if (haveSeenLocation[key]) {
+          haveSeenLocation[key].otherMessages =
+            haveSeenLocation[key].otherMessages || []
+          haveSeenLocation[key].otherMessages.push(message)
+        } else {
+          haveSeenLocation[key] = message
+          all.push(message)
+        }
+        return all
+      }, [])
+    })
+}
